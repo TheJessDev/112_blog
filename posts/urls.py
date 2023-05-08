7441a0912d0864ddec80c66faff78e194be8655c
TheJessDev <1jessicalmorrison@gmail.com>
2023-05-08 00:21:58 -0600

    Final Updated; cannot find error for 'no reverse match, posts_edit'

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,8 @@ from .views import (
     PostListView,
     PostDetailView,
     PostCreateView,
+    PostUpdateView,
+    PostDeleteView
 )    
 # can import entire 'views' from post vs. listing one by one 
 # Just don't forget 'views.' in path
@@ -12,6 +14,8 @@ urlpatterns = [
     path("", PostListView.as_view(), name="list"),
     path("<int:pk>/", PostDetailView.as_view(), name="posts_details"),
     path("new/", PostCreateView.as_view(), name="posts_new"),
+    path("<int:pk>/edit/", PostUpdateView.as_view(), name="posts_edit"),
+    path("<int:pk>/delete/", PostDeleteView.as_view(), name="posts_delete"),
 ]
 
 # can also name url short like this:
--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,10 @@ from django.views.generic import (
     ListView, 
     DetailView,
     CreateView,
+    UpdateView,
+    DeleteView
 )
+from django.urls import reverse_lazy
 from .models import Post
 
 class PostListView(ListView):
@@ -17,3 +20,14 @@ class PostCreateView(CreateView):
     template_name = "posts/new.html"
     model = Post
     fields = ["title", "subtitle", "author", "body"]
+
+class PostUpdateView(UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ["title", "subtitle", "body"]
+
+class PostDeleteView(DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("list")
+
